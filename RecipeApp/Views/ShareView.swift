e7607350336b3b0cfe1a99f9b5dbe61b782eb593
Brian Dilts <brianedilts@gmail.com>
2021-04-08 12:17:00 -0600

    New view added

---
+++ RecipeApp/Views/ShareView.swift
@@ -0,0 +1,20 @@
+//
+//  ShareView.swift
+//  RecipeApp
+//
+//  Created by Brian Dilts on 4/8/21.
+//
+
+import SwiftUI
+
+struct ShareView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShareView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShareView()
+    }
+}
