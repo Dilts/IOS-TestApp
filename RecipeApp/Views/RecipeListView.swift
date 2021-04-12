5ddd6b2250e93bd0c6999f2d64743ede8d3253d6
Brian Dilts <brianedilts@gmail.com>
2021-04-12 11:02:55 -0600

    - change swiftUI list to scrollview, lazyvstack, and foreach - hide navigationbartitle and use text element for the title in the recipe list view

--- RecipeApp/Views/RecipeListView.swift
+++ RecipeApp/Views/RecipeListView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct RecipeListView: View {
     
     // Reference the view model
-//    @ObservedObject var model = RecipeModel()
+    //    @ObservedObject var model = RecipeModel()
     // Remove the @ObservedObject that was getting the data and add a property that calls the data from the RecipeTabView where we set the data as an .environmentObject
     @EnvironmentObject var model:RecipeModel
     
@@ -19,46 +19,61 @@ struct RecipeListView: View {
         
         
         NavigationView {
-            List(model.recipes) { r in
+            VStack(alignment: .leading) {
+                Text("All Recipes")
+                    .bold()
+                    .padding(.top, 40)
+                    .font(.largeTitle)
                 
-                NavigationLink(
-                    destination: RecipeDetailView(recipe: r),
-                    label: {
-                       
-                        // MARK: Row Item
-                        HStack(spacing: 20.0) {
-                            Image(r.image)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 50, height: 50, alignment: .center)
-                                .clipped()
-                                .cornerRadius(5)
-                            Text(r.name)
+                ScrollView {
+                    
+                    LazyVStack (alignment: .leading){
+                        ForEach(model.recipes) { r in
+                            
+                            NavigationLink(
+                                destination: RecipeDetailView(recipe: r),
+                                label: {
+                                    
+                                    // MARK: Row Item
+                                    HStack(spacing: 20.0) {
+                                        Image(r.image)
+                                            .resizable()
+                                            .scaledToFill()
+                                            .frame(width: 50, height: 50, alignment: .center)
+                                            .clipped()
+                                            .cornerRadius(5)
+                                        Text(r.name)
+                                            .foregroundColor(.black)
+                                    }
+                                    
+                                })
+                            
+                            
                         }
-                        
-                    })
-               
-                
+                    }
+                }
             }
-            .navigationBarTitle("All Recipes in App")
+            .navigationBarHidden(true)
+            .padding(.leading)
         }
         
     }
     // Reference the view model
     //@ObservedObject var model = RecipeModel()
     
-//    List(model.recipes) { r in
-//        
-//        HStack {
-//            Image(r.image)
-//            Text (r.name)
-//        }
-//        
-//    }
+    //    List(model.recipes) { r in
+    //
+    //        HStack {
+    //            Image(r.image)
+    //            Text (r.name)
+    //        }
+    //
+    //    }
 }
 
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
